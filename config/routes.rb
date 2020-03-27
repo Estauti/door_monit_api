f92087e038eaf47b55b0e43910b7f0a303234fbc
Estauti <estautib@gmail.com>
2020-03-27 13:06:52 -0300

    Fix routes to accept vue build routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
     resources :devices
     resources :measurements
   end
+
+  match "/*path", to: redirect(path: "/?redirect=%{path}"), constraints: lambda { |params, request| !params[:path].match(/api|rails/) }, via: :all
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
