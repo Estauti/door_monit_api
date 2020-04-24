56dfa6045949fc475ef3ecadc2d0432727c54d0a
Estauti <estautib@gmail.com>
2020-04-23 23:41:52 -0300

    Add refresh and get by token endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   scope :api do
-    # mount_devise_token_auth_for 'User', at: 'auth'
-    # get 'auth/refresh', :to => 'users#refresh'
-    # get 'auth/get_by_token', :to => 'users#get_by_token'
     devise_for :users,
       path: 'auth',
       controllers: {
@@ -10,6 +7,9 @@ Rails.application.routes.draw do
         registrations: 'registrations'
       }, 
       defaults: { format: :json }
+
+    get 'auth/refresh', :to => 'users#refresh'
+    get 'auth/get_by_token', :to => 'users#get_by_token'
 
     resources :devices
     resources :measurements
